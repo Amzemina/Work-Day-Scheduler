4d4a28655cdf42c0f73607e7af7d2166d4ea94a8
Ashley Zemina <amzemina@gmail.com>
2023-07-08 09:39:44 -0400

    added date with js, and added more hours in html

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDayEl = $('#currentDay');
+
+function displayDate() {
+  var rightNow = dayjs().format('MMM DD, YYYY');
+  currentDayEl.text(rightNow);
+  }
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +27,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+displayDate()
